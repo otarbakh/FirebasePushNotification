f6a53cd2480e5e831f7ed4ad99e36206f084ba8d
User <otarbakh@gmail.com>
2023-02-11 00:10:51 +0400

    Added constants from firebase .

---
+++ app/src/main/java/com/example/firebasepushnotification/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.firebasepushnotification
+
+object Constants {
+
+    const val BASE_URL = "https://fcm.googleapis.com"
+    const val CONTENT_TYPE  ="application/json"
+    const val  SERVER_KEY = "AAAAtlIBA3w:APA91bHe2Bg4Vvm7UtMi7TcfYtlPV_GXOCkXQ6yWS_6eDfKDy7Ys6BvL_3DdM1sNYfU54vbf9fDyHygUPcZgZx7Vu7F_h7KW_m3Y0yNTcfnUF521l4FNcbXjS_4Yu5z9qZLSfHYA31t7"
+}
