2a0f8b0a60ec0b6c24d77c43b4f5742cae8a5cc7
User <otarbakh@gmail.com>
2023-02-12 23:42:01 +0400

    notification created

--- app/src/main/java/com/example/firebasepushnotification/FirebaseService.kt
+++ app/src/main/java/com/example/firebasepushnotification/FirebaseService.kt
@@ -1,19 +1,59 @@
 package com.example.firebasepushnotification
 
+import android.annotation.SuppressLint
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_ONE_SHOT
 import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kotlin.random.Random
 
-class FirebaseService:FirebaseMessagingService() {
+private const val CHANNEL_ID = "my_channel"
+
+
+class FirebaseService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-        val intent = Intent(this,MainActivity::class.java)
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notificationID = Random.nextInt()
 
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, FLAG_ONE_SHOT)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(message.data["title"])
+            .setContentTitle(message.data["message"])
+            .setSmallIcon(R.drawable.ic_baseline_lightbulb)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(notificationID,notification)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
